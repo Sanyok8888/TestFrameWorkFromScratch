b579f4c6aa3984b23ccc6fcad809b02b1b423541
otupikov <otupikov@star.global>
2022-08-11 19:14:16 +0300

    Updated chromedriver.exe

--- src/test/java/alluretheinternetloginpagehometask/BaseTest.java
+++ src/test/java/alluretheinternetloginpagehometask/BaseTest.java
@@ -3,7 +3,6 @@ package alluretheinternetloginpagehometask;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
